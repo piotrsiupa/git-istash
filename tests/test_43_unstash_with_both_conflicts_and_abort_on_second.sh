ff989b742a6ca5262a05de2dcbc68969fafa7c88
Piotr Siupa <piotr.siupa+stash@gmail.com>
2024-06-20 00:32:37 +0200

    Added tests for aborted runs with conflicts

---
+++ tests/test_43_unstash_with_both_conflicts_and_abort_on_second.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env sh
+
+set -e
+
+cd "$1"
+
+git branch -m branch0
+printf 'aaa\n' >aaa
+git add aaa
+git commit -m 'Added aaa'
+
+printf 'bbb\n' >aaa
+git add aaa
+printf 'ccc\n' >aaa
+git stash push
+
+git switch -c branch1
+printf 'ddd\n' >aaa
+git commit -am 'Changed aaa'
+
+temp_file="$(mktemp)"
+if git unstash 2>"$temp_file"
+then
+	rm -f "$temp_file"
+	exit 1
+fi
+text="$(cat "$temp_file" | tail -n4)"
+rm -f "$temp_file"
+test "$text" = '
+hint: Disregard all hints above about using "git rebase".
+hint: Use "git unstash --continue" after fixing conflicts.
+hint: To abort and get back to the state before "git unstash", run "git unstash --abort".'
+test "$(git status --porcelain)" = 'UU aaa'
+
+printf 'eee\n' >aaa
+git add aaa
+temp_file="$(mktemp)"
+if git unstash --continue 2>"$temp_file"
+then
+	rm -f "$temp_file"
+	exit 1
+fi
+text="$(cat "$temp_file" | tail -n4)"
+rm -f "$temp_file"
+test "$text" = '
+hint: Disregard all hints above about using "git rebase".
+hint: Use "git unstash --continue" after fixing conflicts.
+hint: To abort and get back to the state before "git unstash", run "git unstash --abort".'
+test "$(git status --porcelain)" = 'UU aaa'
+
+git unstash --abort
+test "$(git status --porcelain)" = ''
+test "$(git show :aaa)" = 'ddd'
+test "$(cat aaa)" = 'ddd'
