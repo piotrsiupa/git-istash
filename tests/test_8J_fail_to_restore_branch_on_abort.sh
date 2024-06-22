74033afe41b379c06de3d2e129b664ee0deb1857
Piotr Siupa <piotr.siupa+stash@gmail.com>
2024-06-22 10:42:39 +0200

    Added tests for miscellaneous errors
    Also removed an error when `HEAD` is currently on the stash that is
    poped because technically there is no reason to disallow it.

---
+++ tests/test_8J_fail_to_restore_branch_on_abort.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env sh
+
+set -e
+
+cd "$1"
+
+git branch -m branch0
+printf 'aaa\n' >aaa
+git add aaa
+git commit -m 'Added aaa'
+
+printf 'bbb\n' >aaa
+git stash push
+
+git switch -c branch1
+printf 'ddd\n' >aaa
+git commit -am 'Changed aaa'
+
+temp_file="$(mktemp)"
+if git unstash 2>"$temp_file"
+then
+	rm -f "$temp_file"
+	exit 1
+fi
+text="$(tail -n4 <"$temp_file")"
+rm -f "$temp_file"
+test "$text" = '
+hint: Disregard all hints above about using "git rebase".
+hint: Use "git unstash --continue" after fixing conflicts.
+hint: To abort and get back to the state before "git unstash", run "git unstash --abort".'
+test "$(git status --porcelain)" = 'UU aaa'
+test "$(git rev-list --walk-reflogs --count --ignore-missing refs/stash)" -eq 1
+
+branch1_hash="$(git rev-parse branch1)"
+git branch -D branch1
+if git unstash --abort ; then exit 1 ; fi
+
+git branch branch1 "$branch1_hash"
+git unstash --abort
+test "$(git status --porcelain)" = ''
+test "$(git show :aaa)" = 'ddd'
+test "$(cat aaa)" = 'ddd'
+test "$(git rev-list --walk-reflogs --count --ignore-missing refs/stash)" -eq 1
