77548054891e2ea61ded396d5a26bff9d26850c7
Piotr Siupa <piotr.siupa+stash@gmail.com>
2024-06-23 12:02:01 +0200

    Added `--debug` and more colors to `tests/run.sh`
    The option `--debug` makes stdout and stderr of the tests scripts be not
    to be supressed.
    Stderr is now colored red and some other colors were also adjusted.
    
    Additionally, the script now uses super-advanced tricks with named pipes
    instead unreliable greping the output of commands.

--- tests/run.sh
+++ tests/run.sh
@@ -3,7 +3,6 @@
 set -e
 
 cd "$(dirname "$0")"
-results_file='./results.txt'
 scripts_dir='../scripts'
 
 get_test_script() { # test_name
@@ -24,46 +23,103 @@ create_test_dir() { # test_name
 	git -C "$test_dir" commit --allow-empty -m 'Initial commit'
 }
 
-run_test() { # test_name
-	cleanup_test "$1"
-	create_test_dir "$1" 1>/dev/null
-	if "./$(get_test_script "$1")" "$(get_test_dir "$1")" 1>/dev/null 2>&1
-	then
-		printf '\e[32mPASSED - "%s"\e[0m\n' "$(printf '%s' "$1" | tr '_' ' ')"
-		cleanup_test "$1"
-	else
-		printf '\e[31mFAILED - "%s"\e[0m (the result is kept)\n' "$(printf '%s' "$1" | tr '_' ' ')"
-	fi
-}
-run_tests() { # pattern
+find_tests() { # pattern
 	find . -maxdepth 1 -type f -name 'test_*.sh' -print0 \
 	| xargs -r0n1 -- basename | rev | cut -c4- | rev | cut -c6- \
 	| grep -P "$1" \
-	| sort \
-	| while read -r test_name
-	do
-		run_test "$test_name"
-	done
+	| sort
 }
 
-print_summary() { # results_file
-	total_tests="$(wc -l <"$1")"
-	passed_tests="$(grep -c 'PASSED' <"$1" || true)"
-	if [ "$total_tests" -ne 0 ]
+run_test() { # test_name
+	cleanup_test "$1"
+	create_test_dir "$1" 1>/dev/null
+	if [ "$debug" -eq 0 ]
 	then
-		printf -- '----------------\n'
-		if [ "$passed_tests" -eq "$total_tests" ]
+		test_passed=0
+		if "./$(get_test_script "$1")" "$(get_test_dir "$1")" 1>/dev/null 2>&1
 		then
-			printf '\e[32m'
-		else
-			printf '\e[31m'
+			test_passed=1
 		fi
-		printf 'Passed %i out of %i tests.\e[0m\n' "$passed_tests" "$total_tests"
 	else
-		printf '\e[33mNo matching tests were found!\e[0m\n'
+		pipe_file="$(mktemp -u)"
+		mkfifo "$pipe_file"
+		exec 4<>"$pipe_file"
+		rm "$pipe_file"
+		{
+			if "./$(get_test_script "$1")" "$(get_test_dir "$1")"
+			then
+				printf '1\n' 1>&4
+			else
+				printf '0\n' 1>&4
+			fi 5>&2 2>&1 1>&5 5>&- \
+			| if [ -t 1 ]
+			then
+				sed -u 's/^.*$/\t\x1B[31m&\x1B[39m/'
+			else
+				sed -u 's/^/\t/'
+			fi
+		} 5>&2 2>&1 1>&5 5>&- | sed -u 's/^/\t/'
+		read -r test_passed <&4
+		exec 4>&-
+	fi
+	if [ "$test_passed" -ne 0 ]
+	then
+		printf '\e[0;1;32m'
+		printf 'PASSED - "%s"' "$(printf '%s' "$1" | tr '_' ' ')"
+		printf '\e[22;39m\n'
+		cleanup_test "$1"
+		return 0
+	else
+		printf '\e[0;1;31m'
+		printf 'FAILED - "%s"' "$(printf '%s' "$1" | tr '_' ' ')"
+		printf '\e[22;39m'
+		printf ' (the result is kept)\n'
+		return 1
 	fi
 }
+run_tests() { # pattern
+	pipe_file="$(mktemp -u)"
+	mkfifo "$pipe_file"
+	exec 3<>"$pipe_file"
+	rm "$pipe_file"
+	find_tests "$1" \
+	| while read -r test_name
+	do
+		if run_test "$test_name"
+		then
+			printf '1' 1>&3
+		fi
+	done
+	printf '\n' 1>&3
+	passed_tests="$(head -n 1 <&3 | tr -d '\n' | wc -c)"
+	exec 3>&-
+}
 
+print_summary() {
+	if [ "$total_tests" -ne 0 ]
+	then
+		if [ "$passed_tests" -eq "$total_tests" ]
+		then
+			printf '\e[42;1;32;4m'
+			printf 'Passed all %i tests.' "$total_tests"
+		else
+			printf '\e[41;2;31;4m'
+			printf 'Passed %i out of %i tests.' "$passed_tests" "$total_tests"
+		fi
+	else
+		printf '\e[43;1;33;4m'
+		printf 'No matching tests were found!'
+	fi
+	printf '\e[0m'
+	printf '\n'
+}
+
+debug=0
+if [ "$1" = '--debug' ]
+then
+	debug=1
+	shift
+fi
 if [ $# -eq 0 ]
 then
 	filter=''
@@ -79,7 +135,7 @@ fi
 
 test -d "$scripts_dir"
 PATH="$(realpath "$scripts_dir"):$PATH"
+total_tests="$(find_tests "$filter" | wc -l)"
 export PATH
-run_tests "$filter" | tee "$results_file"
-print_summary "$results_file"
-rm -f "$results_file"
+run_tests "$filter"
+print_summary
