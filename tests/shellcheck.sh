1ebbe0b1ba6b09e2f3146b922bfd35a9c0100c0e
Piotr Siupa <piotr.siupa+stash@gmail.com>
2024-06-27 08:44:03 +0200

    Added `--help` to `tests/shellcheck.sh`

--- tests/shellcheck.sh
+++ tests/shellcheck.sh
@@ -2,6 +2,22 @@
 
 set -e
 
+if [ "$1" = '-h' ] || [ "$1" = '--help' ]
+then
+	printf '%s - Test script that runs "shellcheck" on all shell scripts in this\n    repository. ' "$(basename "$0")"
+	printf '(All scripts from directories "scripts" AND "tests".)\n'
+	printf '\n'
+	printf 'Usage: %s [--help]\n' "$(basename "$0")"
+	printf '\n'
+	printf 'Options:\n'
+	printf '    -h, --help\t- Print this help message and exit.\n'
+	exit 0
+elif [ $# -ne 0 ]
+then
+	printf 'Unrecognised argument "%s".\n' "$1" 1>&2
+	exit 1
+fi
+
 cd "$(dirname "$0")"
 
 list_files() {
