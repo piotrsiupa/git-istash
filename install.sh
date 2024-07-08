6001577560040690fc3ba8031b6b7dd63f5ea98c
Piotr Siupa <piotr.siupa+stash@gmail.com>
2024-07-08 09:36:30 +0200

    Refactored the `install.sh`

--- install.sh
+++ install.sh
@@ -2,16 +2,12 @@
 
 set -e
 
-source_path='./scripts'
-target_path_relative_to_home='.local/bin'
-target_path="$HOME/$target_path_relative_to_home"
-profile_path="$HOME/.profile"
-
 print_help() {
-	printf '%s - A simple installation script.\n' "$(basename "$0")"
+	printf '%s - An installation script for "git istash".\n' "$(basename "$0")"
 	#shellcheck disable=SC2016
-	printf '    Basically, all it does is copying the files to "%s" and\n    making sure this directory is in "$PATH" (by editing "%s").\n' "\$HOME/$target_path_relative_to_home" "\$HOME/.profile"
-	printf '    (Hardcore terminal dwellers will probably want to do it their way but this\n    is more than good enough for a casual user.)\n'
+	printf '    It copies files to the appropriate places and sets up the PATH variable if\n    needed. '
+	printf     'With the flag "--uninstall", it can also undo the changes it makes.\n'
+	printf '    (Hardcore terminal dwellers will may want to do it their own way but for\n    everyone else this script should be more than good enough.)\n'
 	printf '\n'
 	printf 'usage: %s [<option>...]\n' "$(basename "$0")"
 	printf '\n'
@@ -20,35 +16,185 @@ print_help() {
 	printf '    -u, --uninstall\t- Undo all the changes that the install script would\n\t\t\t  made without this flag.\n'
 }
 
+make_target_path() { # source_path (temporarily unused)
+	# This will have more sense after the directories in the project are reorganized.
+	printf '%s/.local/bin' "$HOME"
+}
+
+make_profile_path() {
+	printf '%s/.profile' "$HOME"
+}
+
+switch_prefix() { # old_prefix new_prefix string
+	printf '%s' "$2"
+	printf '%s' "$3" | cut -c$((${#1} + 1))-
+}
+
+are_files_correct_in_target() { # source_path
+	source_path="$1"
+	target_path="$(make_target_path "$source_path")"
+	test "$(
+			find "$source_path" -type f \
+			| while read -r f
+			do
+				if ! cmp -s "$f" "$(switch_prefix "$source_path" "$target_path" "$f")"
+				then
+					printf 'x'
+					break
+				fi
+			done
+		)" != 'x'
+}
+
+are_knows_files_in_target() { # source_path
+	source_path="$1"
+	target_path="$(make_target_path "$source_path")"
+	test "$(
+			find "$source_path" -type f \
+			| while read -r f
+			do
+				if [ -e "$(switch_prefix "$source_path" "$target_path" "$f")" ]
+				then
+					printf 'x'
+					break
+				fi
+			done
+		)" = 'x'
+}
+
+are_foreign_files_in_target() { # source_path
+	source_path="$1"
+	target_path="$(make_target_path "$source_path")"
+	test "$(
+			find "$target_path" -type f \
+			| while read -r f
+			do
+				if [ ! -e "$(switch_prefix "$target_path" "$source_path" "$f")" ]
+				then
+					printf 'x'
+					break
+				fi
+			done
+		)" = 'x'
+}
+
+is_target_in_PATH() { # source_path
+	printf '%s' "$PATH" | tr ':' '\n' | grep -qFx "$(make_target_path "$1")"
+}
+
+make_profile_insertion() { # path
+	printf '\n'
+	printf '# add user'\''s private bin directory to PATH\n'
+	#shellcheck disable=SC2016
+	printf 'export PATH="%s:$PATH"\n' "$1"
+}
+
+get_task_path() { # task
+	printf '%s' "$1" | cut -d_ -f2-
+}
+
+make_create_directory_task() { # source_path
+	source_path="$1"
+	target_path="$(make_target_path "$source_path")"
+	if [ ! -e "$target_path" ] || [ "$debug" = y ]
+	then
+		printf 'mkdir_%s\n' "$source_path"
+	elif [ ! -d "$target_path" ]
+	then
+		printf 'fatal: "%s" already exists but it'\''s not a directory.\n' "$target_path" 1>&2
+		return 1
+	fi
+}
+print_create_directory_task() { # source_path
+	printf 'A directory "%s" will be created.\n' "$(make_target_path "$1")"
+}
+execute_create_directory_task() { # source_path
+	mkdir -p "$(make_target_path "$1")"
+}
+
+make_remove_directory_task() { # source_path
+	if ! are_foreign_files_in_target "$1" || [ "$debug" = y ]
+	then
+		printf 'rmdir_%s\n' "$1"
+	fi
+}
+print_remove_directory_task() { # source_path
+	printf 'The (now empty) directory "%s" will be removed.\n' "$(make_target_path "$1")"
+}
+execute_remove_directory_task() { # source_path
+	rmdir -p "$(make_target_path "$1")"
+}
+
+make_copy_files_task() { # source_path
+	if ! are_files_correct_in_target "$1" || [ "$debug" = y ]
+	then
+		printf 'copy_%s\n' "$1"
+	fi
+}
+print_copy_files_task() { # source_path
+	printf 'The contents of the directory "%s" will be copied to "%s".\n' "$1" "$(make_target_path "$1")"
+}
+execute_copy_files_task() { # source_path
+	cp -fr "$1"/* "$(make_target_path "$1")"
+}
+
+make_remove_files_task() { # source_path
+	if are_knows_files_in_target "$1" || [ "$debug" = y ]
+	then
+		printf 'delete_%s\n' "$1"
+	fi
+}
+print_remove_files_task() { # source_path
+	printf 'The contents of the directory "%s" will be deleted from "%s".\n' "$1" "$(make_target_path "$1")"
+}
+execute_remove_files_task() { # source_path
+	source_path="$1"
+	target_path="$(make_target_path "$source_path")"
+	find "$source_path" -mindepth 1 -maxdepth 1 \
+	| while read -r f
+	do
+		rm -rf '"%s"\n' "$(switch_prefix "$source_path" "$target_path" "$f")"
+	done
+}
+
+make_add_to_profile_task() { # source_path
+	if ! is_target_in_PATH "$1" || [ "$debug" = y ]
+	then
+		printf 'add-path_%s\n' "$1"
+	fi
+}
+print_add_to_profile_task() { # source_path
+	printf '"%s" will be added to PATH by appending the "%s".\n' "$(make_target_path "$1")" "$(make_profile_path)"
+}
+execute_add_to_profile_task() { # source_path
+	make_profile_insertion "$(make_target_path "$1")" >>"$(make_profile_path)"
+}
+
+make_remove_from_profile_task() { # source_path
+	if ( is_target_in_PATH "$1" && ! are_foreign_files_in_target "$1" ) || [ "$debug" = y ]
+	then
+		printf 'remove-path_%s\n' "$1"
+	fi
+}
+print_remove_from_profile_task() { # source_path
+	printf 'The line in "%s" that appends "%s" to PATH will be removed.\n' "$(make_profile_path)" "$(make_target_path "$1")"
+}
+execute_remove_from_profile_task() { # source_path
+	delete_regex='\(^\|\n\?\n\)\([\t ]*#[^\n]*\n\)\?\s*'"$(make_profile_insertion "$(make_target_path "$1")" | tail -n 1 | head -c -1 | sed -e 's/[^^]/[&]/g' -e 's/\^/\\^/g')"'[\t ]*'
+	sed -i -e ':s;$!{N;bs}' -e "\$s;$delete_regex;;" "$(make_profile_path)"
+}
+
 gather_tasks() {
 	tasks="$(
 		if [ "$uninstall" = n ]
 		then
-			if [ ! -e "$target_path" ]
-			then
-				printf 'create-home-local-bin\n'
-			fi
-			if [ -n "$(find "$source_path" -type f | while read -r script ; do if ! cmp -s "$script" "$target_path$(printf '%s' "$script" | cut -c$((${#source_path} + 1))-)" ; then printf 'c' ; fi ; done)" ]
-			then
-				printf 'copy-to-home-local-bin\n'
-			fi
-			if ! printf '%s' "$PATH" | tr ':' '\n' | grep -qFx "$HOME/.local/bin"
-			then
-				printf 'add-home-local-bin-to-path\n'
-			fi
+			make_create_directory_task 'scripts'
+			make_copy_files_task 'scripts'
+			make_add_to_profile_task 'scripts'
 		else
-			if [ -z "$(find "$target_path" -type f | while read -r f ; do if [ ! -e "$source_path$(printf '%s\n' "$f" | cut -c$((${#target_path} + 1))-)" ] ; then printf 'x' ; break ; fi ; done)" ]
-			then
-				printf 'remove-home-local-bin-from-path\n'
-			fi
-			if [ -n "$(find "$source_path" -type f | while read -r script ; do if [ -e "$target_path$(printf '%s' "$script" | cut -c$((${#source_path} + 1))-)" ] ; then printf 'e' ; fi ; done)" ]
-			then
-				printf 'delete-from-home-local-bin\n'
-			fi
-			if [ -z "$(find "$target_path" -type f | while read -r f ; do if [ ! -e "$source_path$(printf '%s\n' "$f" | cut -c$((${#target_path} + 1))-)" ] ; then printf 'x' ; break ; fi ; done)" ]
-			then
-				printf 'remove-home-local-bin\n'
-			fi
+			make_remove_from_profile_task 'scripts'
+			make_remove_files_task 'scripts'
+			make_remove_directory_task 'scripts'
 		fi
 	)"
 }
@@ -59,34 +205,22 @@ show_tasks() {
 	printf '%s\n' "$tasks" \
 	| while read -r task
 	do
-		printf '  - '
+		printf ' - '
+		source_path="$(get_task_path "$task")"
 		case "$task" in
-			create-home-local-bin)
-				printf 'A directory "%s" will be created.\n' "$target_path"
-				;;
-			copy-to-home-local-bin)
-				printf 'The contents of the directory "%s" will be copied to\n    "%s".\n' "$source_path" "$target_path"
-				;;
-			add-home-local-bin-to-path)
-				#shellcheck disable=SC2016
-				printf '"%s" will be added to "$PATH" by appending\n    a line to the "%s".\n' "$target_path" "$profile_path"
-				;;
-			remove-home-local-bin-from-path)
-				#shellcheck disable=SC2016
-				printf 'The line in "%s" that appends\n    "%s" to "$PATH" will be removed.\n' "$profile_path" "$target_path"
-				;;
-			delete-from-home-local-bin)
-				printf 'The contents of the directory "%s" will be deleted from\n    "%s".\n' "$source_path" "$target_path"
-				;;
-			remove-home-local-bin)
-				printf 'The (now empty) directory "%s" will be removed.\n' "$target_path"
-				;;
-			*)
-				printf 'fatal: Unknown task "%s".' "$task" 1>&2
-				exit 1
-				;;
+			mkdir_*) print_create_directory_task "$source_path" ;;
+			copy_*) print_copy_files_task "$source_path" ;;
+			add-path_*) print_add_to_profile_task "$source_path" ;;
+			rmdir_*) print_remove_directory_task "$source_path" ;;
+			delete_*) print_remove_files_task "$source_path" ;;
+			remove-path_*) print_remove_from_profile_task "$source_path" ;;
+			*) printf 'fatal: Unknown task "%s".' "$task" 1>&2 ; exit 1 ;;
 		esac
 	done
+	if [ "$debug" = y ]
+	then
+		printf '\nWARNING: This is a debug mode. Tasks were not validated!\n(All of them were indiscriminately put on the list above.)\n' 1>&2
+	fi
 }
 
 ask_confirmation() {
@@ -106,45 +240,19 @@ ask_confirmation() {
 	return 1
 }
 
-print_export_path() {
-	printf '\n'
-	printf '# add user'\''s private bin directory to PATH\n'
-	#shellcheck disable=SC2016
-	printf 'export PATH="$HOME/%s:$PATH"\n' "$target_path_relative_to_home"
-}
-
 run_tasks() {
 	printf '%s\n' "$tasks" \
 	| while read -r task
 	do
+		source_path="$(get_task_path "$task")"
 		case "$task" in
-			create-home-local-bin)
-				mkdir -p "$target_path"
-				;;
-			copy-to-home-local-bin)
-				cp -fr "$source_path"/* "$target_path"
-				;;
-			add-home-local-bin-to-path)
-				print_export_path >>"$profile_path"
-				;;
-			remove-home-local-bin-from-path)
-				delete_regex='\(^\|\n\?\n\)\([\t ]*#[^\n]*\n\)\?\s*'"$(print_export_path | tail -n 1 | head -c -1 | sed -e 's/[^^]/[&]/g' -e 's/\^/\\^/g')"'[\t ]*'
-				sed -i -e ':s;$!{N;bs}' -e "\$s;$delete_regex;;" "$profile_path"
-				;;
-			delete-from-home-local-bin)
-				find "$source_path" -type f \
-				| while read -r script
-				do
-					rm -f '"%s"\n' "$target_path$(printf '%s' "$script" | cut -c$((${#source_path} + 1))-)"
-				done
-				;;
-			remove-home-local-bin)
-				rmdir -p "$target_path"
-				;;
-			*)
-				printf 'fatal: Unknown task "%s"!' "$task" 1>&2
-				exit 1
-				;;
+			mkdir_*) execute_create_directory_task "$source_path" ;;
+			copy_*) execute_copy_files_task "$source_path" ;;
+			add-path_*) execute_add_to_profile_task "$source_path" ;;
+			rmdir_*) execute_remove_directory_task "$source_path" ;;
+			delete_*) execute_remove_files_task "$source_path" ;;
+			remove-path_*) execute_remove_from_profile_task "$source_path" ;;
+			*) printf 'fatal: Unknown task "%s".' "$task" 1>&2 ; exit 1 ;;
 		esac
 	done
 }
@@ -158,10 +266,24 @@ do_the_install_thing() {
 		then
 			run_tasks
 			printf '\n'
-			printf 'Installation finished successfully.\n'
+			if [ "$uninstall" = n ]
+			then
+				printf 'Installation finished successfully.\n'
+			else
+				printf 'Uninstall finished successfully.\n'
+			fi
+			if printf '%s\n' "$tasks" | grep -q '^\(add\|remove\)-path_'
+			then
+				printf '\nPATH was modified. Restart the session to apply those changes.\n'
+			fi
 			exit 0
 		else
-			printf 'Installation aborted\n'
+			if [ "$uninstall" = n ]
+			then
+				printf 'Installation aborted\n'
+			else
+				printf 'Uninstall aborted\n'
+			fi
 			printf 'Nothing was changed in the system.\n'
 			exit 2
 		fi
@@ -176,9 +298,10 @@ do_the_install_thing() {
 	fi
 }
 
-getopt_result="$(getopt -o'hu' --long='help,uninstall' -n"$(basename "$0")" -- "$@")"
+getopt_result="$(getopt -o'hu' --long='help,uninstall,debug' -n"$(basename "$0")" -- "$@")"
 eval set -- "$getopt_result"
 uninstall=n
+debug=n
 while true
 do
 	case "$1" in
@@ -188,6 +311,9 @@ do
 		;;
 	-u|--uninstall)
 		uninstall=y
+		;;
+	--debug)
+		debug=y
 		;;
 	--)
 		shift
