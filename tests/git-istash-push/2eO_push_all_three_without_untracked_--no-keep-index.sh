5062a5cd714e256efbd3f35540639749a8f53c90
Piotr Siupa <piotr.siupa+stash@gmail.com>
2024-07-27 02:38:30 +0200

    Added tests for `--no-include-untracked`

---
+++ tests/git-istash-push/2eO_push_all_three_without_untracked_--no-keep-index.sh
@@ -0,0 +1,34 @@
+. "$(dirname "$0")/../commons.sh" 1>/dev/null
+
+git switch --orphan ooo
+
+printf 'bbb\n' >aaa
+git add aaa
+printf 'ccc\n' >aaa
+printf 'ddd\n' >ddd
+assert_exit_code 0 git istash push --no-include-untracked --no-keep-index
+assert_all_files 'ddd|ignored'
+assert_status '?? ddd'
+assert_file_contents ddd 'ddd'
+assert_file_contents ignored 'ignored'
+assert_stash_count 1
+assert_branch_count 1
+assert_head_name '~ooo'
+assert_data_files 'none'
+assert_rebase n
+
+git switch master
+
+assert_exit_code 0 git stash pop --index
+assert_all_files 'aaa|ddd|ignored'
+assert_tracked_files ''
+assert_status 'AM aaa|?? ddd'
+assert_file_contents aaa 'ccc' 'bbb'
+assert_file_contents ddd 'ddd'
+assert_file_contents ignored 'ignored'
+assert_stash_count 0
+assert_log_length 1
+assert_branch_count 1
+assert_head_name 'master'
+assert_data_files 'none'
+assert_rebase n
---
+++ tests/git-istash-push/3eB_push_all_three_without_untracked_--keep-index.sh
@@ -0,0 +1,41 @@
+. "$(dirname "$0")/../commons.sh" 1>/dev/null
+
+printf 'aaa\n' >aaa
+git add aaa
+git commit -m 'Added aaa'
+
+printf 'bbb\n' >aaa
+git add aaa
+printf 'ccc\n' >aaa
+printf 'ddd\n' >ddd
+correct_head_hash="$(git rev-parse HEAD)"
+assert_exit_code 0 git istash push --no-include-untracked --keep-index
+assert_all_files 'aaa|ddd|ignored'
+assert_tracked_files 'aaa'
+assert_status 'M  aaa|?? ddd'
+assert_file_contents aaa 'bbb' 'bbb'
+assert_file_contents ddd 'ddd'
+assert_file_contents ignored 'ignored'
+assert_stash_count 1
+assert_log_length 2
+assert_branch_count 1
+assert_head_hash "$correct_head_hash"
+assert_head_name 'master'
+assert_data_files 'none'
+assert_rebase n
+
+git reset --hard
+
+assert_exit_code 0 git stash pop --index
+assert_all_files 'aaa|ddd|ignored'
+assert_tracked_files 'aaa'
+assert_status 'MM aaa|?? ddd'
+assert_file_contents aaa 'ccc' 'bbb'
+assert_file_contents ddd 'ddd'
+assert_file_contents ignored 'ignored'
+assert_stash_count 0
+assert_log_length 2
+assert_branch_count 1
+assert_head_name 'master'
+assert_data_files 'none'
+assert_rebase n
