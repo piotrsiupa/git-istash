13a54b13f112193abaa7e5acbf7f749e96b0119a
Piotr Siupa <piotr.siupa+stash@gmail.com>
2024-09-21 20:35:54 +0200

    Added tests for applying stash with ignored files

---
+++ tests/applying/2F_ignored_files_stashed_with_option_all.sh
@@ -0,0 +1,37 @@
+. "$(dirname "$0")/../commons.sh" 1>/dev/null
+
+PARAMETRIZE_HEAD_TYPE 'BRANCH' 'DETACH'
+PARAMETRIZE_APPLY_POP
+
+__test_section__ 'Prepare repository'
+printf 'aaa\n' >aaa
+git add aaa
+git commit -m 'Added aaa'
+
+__test_section__ 'Create stash'
+printf 'bbb\n' >aaa
+git add aaa
+printf 'ccc\n' >aaa
+printf 'zzz\n' >zzz
+git stash push -ua
+
+__test_section__ 'Create "conflict"'
+printf 'different contents\n' >ignored
+
+SWITCH_HEAD_TYPE
+
+__test_section__ "$CAP_OPERATION stash"
+correct_head_hash="$(get_head_hash_H)"
+assert_exit_code 0 capture_outputs git istash "$OPERATION"
+assert_files_H '
+MM aaa		ccc	bbb
+?? zzz		zzz
+!! ignored	ignored
+'
+assert_stash_count_O 1
+assert_log_length_H 2
+assert_branch_count 1
+assert_head_hash_H "$correct_head_hash"
+assert_head_name_H
+assert_data_files 'none'
+assert_rebase n
