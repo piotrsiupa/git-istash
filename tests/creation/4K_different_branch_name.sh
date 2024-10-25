e9dd106565dba613dce430cdd81b69510bb4dbec
Piotr Siupa <piotr.siupa+stash@gmail.com>
2024-10-25 18:59:25 +0200

    A very basic initial implementation of `push`
    It cannot handle any options yet.

---
+++ tests/creation/4K_different_branch_name.sh
@@ -0,0 +1,71 @@
+. "$(dirname "$0")/../commons.sh" 1>/dev/null
+
+non_essential_test
+
+PARAMETRIZE_HEAD_TYPE 'BRANCH' 'ORPHAN'
+PARAMETRIZE_ALL 'DEFAULT'
+PARAMETRIZE_UNTRACKED 'DEFAULT'
+PARAMETRIZE_KEEP_INDEX
+
+correct_head_hash="$(get_head_hash)"
+SWITCH_HEAD_TYPE
+git branch -m 'new-and-cool-branch'
+
+__test_section__ 'Create stash'
+printf 'bbb\n' >aaa
+git add aaa
+assert_exit_code 0 git istash push $KEEP_INDEX_FLAGS $ALL_FLAGS $UNTRACKED_FLAGS
+if ! IS_KEEP_INDEX_ON
+then
+	assert_files_H '
+	!! ignored0	ignored0
+	!! ignored1	ignored1
+	'
+else
+	assert_files_H '
+	A  aaa			bbb
+	!! ignored0	ignored0
+	!! ignored1	ignored1
+	'
+fi
+assert_stash 0 'new-and-cool-branch' '' '
+A  aaa		bbb
+'
+if ! IS_HEAD_ORPHAN
+then
+	assert_stash_base 0 'HEAD'
+else
+	assert_stash_base 0 '~new-and-cool-branch'
+fi
+assert_stash_count 1
+assert_log_length_H 1
+assert_branch_count 1
+assert_head_hash_H "$correct_head_hash"
+if ! IS_HEAD_ORPHAN
+then
+	assert_head_name 'new-and-cool-branch'
+else
+	assert_head_name '~new-and-cool-branch'
+fi
+assert_rebase n
+
+git reset --hard
+
+__test_section__ 'Pop stash'
+assert_exit_code 0 git stash pop --index
+assert_files '
+A  aaa		bbb
+!! ignored0	ignored0
+!! ignored1	ignored1
+'
+assert_stash_count 0
+assert_log_length_H 1
+assert_branch_count 1
+assert_head_hash_H "$correct_head_hash"
+if ! IS_HEAD_ORPHAN
+then
+	assert_head_name 'new-and-cool-branch'
+else
+	assert_head_name '~new-and-cool-branch'
+fi
+assert_rebase n
