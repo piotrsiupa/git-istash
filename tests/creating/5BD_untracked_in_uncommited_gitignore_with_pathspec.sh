37966b208ad5065b45eaf5cdd66280603644cecc
Piotr Siupa <piotr.siupa+stash@gmail.com>
2025-01-01 13:52:35 +0100

    Added tests for stashing `.gitignore`

---
+++ tests/creating/5BD_untracked_in_uncommited_gitignore_with_pathspec.sh
@@ -0,0 +1,96 @@
+. "$(dirname "$0")/../commons.sh" 1>/dev/null
+
+PARAMETRIZE_HEAD_TYPE 'BRANCH' 'DETACH'
+PARAMETRIZE_ALL 'DEFAULT'
+PARAMETRIZE_UNTRACKED 'YES'
+PARAMETRIZE_KEEP_INDEX
+PARAMETRIZE_PATHSPEC_STYLE
+PARAMETRIZE_OPTIONS_INDICATOR IS_PATHSPEC_IN_ARGS
+
+__test_section__ 'Prepare repository'
+printf 'X' >.gitignore
+git add .
+git commit -m 'Added an empty .gitignore'
+
+correct_head_hash="$(get_head_hash)"
+SWITCH_HEAD_TYPE
+
+__test_section__ 'Create stash'
+printf 'aaa\n' >aaa0
+printf 'aaa\n' >aaa1
+printf 'bbb\n' >bbb0
+printf 'bbb\n' >bbb1
+printf 'aaa?\n' >.gitignore
+git add .gitignore
+if ! IS_PATHSPEC_NULL_SEP
+then
+	printf '*1 .gitignore ' | tr ' ' '\n' >.git/pathspec_for_test
+else
+	printf '*1 .gitignore ' | tr ' ' '\0' >.git/pathspec_for_test
+fi
+if IS_PATHSPEC_IN_ARGS
+then
+	#shellcheck disable=SC2086
+	assert_exit_code 0 git istash push $UNTRACKED_FLAGS $ALL_FLAGS $KEEP_INDEX_FLAGS -mX $EOI '*1' '.gitignore'
+elif IS_PATHSPEC_IN_STDIN
+then
+	#shellcheck disable=SC2086
+	assert_exit_code 0 git istash push $UNTRACKED_FLAGS $ALL_FLAGS $KEEP_INDEX_FLAGS -mX $PATHSPEC_NULL_FLAGS --pathspec-from-file=- <.git/pathspec_for_test
+else
+	#shellcheck disable=SC2086
+	assert_exit_code 0 git istash push $UNTRACKED_FLAGS $ALL_FLAGS $KEEP_INDEX_FLAGS -mX $PATHSPEC_NULL_FLAGS --pathspec-from-file .git/pathspec_for_test
+fi
+if ! IS_KEEP_INDEX_ON
+then
+	assert_files_H '
+	   .gitignore	X
+	?? aaa0		aaa
+	?? aaa1		aaa
+	?? bbb0		bbb
+	!! ignored0	ignored0
+	!! ignored1	ignored1
+	'
+else
+	assert_files_H '
+	M  .gitignore	aaa?
+	!! aaa0		aaa
+	!! aaa1		aaa
+	?? bbb0		bbb
+	!! ignored0	ignored0
+	!! ignored1	ignored1
+	'
+fi
+assert_stash_H 0 'X' '
+M  .gitignore	aaa?
+?? bbb1		bbb
+'
+assert_stash_base_H 0 'HEAD'
+assert_stash_count 1
+assert_log_length_H 2
+assert_branch_count 1
+assert_head_hash_H "$correct_head_hash"
+assert_head_name_H
+assert_rebase n
+assert_branch_metadata_H
+
+git reset --hard
+RESTORE_HEAD_TYPE
+
+__test_section__ 'Pop stash'
+assert_exit_code 0 git stash pop --index
+assert_files '
+M  .gitignore	aaa?
+!! aaa0		aaa
+!! aaa1		aaa
+?? bbb0		bbb
+?? bbb1		bbb
+!! ignored0	ignored0
+!! ignored1	ignored1
+'
+assert_stash_count 0
+assert_log_length 2
+assert_branch_count 1
+assert_head_hash "$correct_head_hash"
+assert_head_name 'master'
+assert_rebase n
+assert_branch_metadata_H
