a49a66418510103c03de6e40265c7fd9be00e920
Piotr Siupa <piotr.siupa+stash@gmail.com>
2025-03-12 17:59:39 +0100

    Added tests for weird file names in conflicts
    (Again, it turned out to work correctly.)

---
+++ tests/applying/8BC_weird_characters_in_file_names_in_conflicts.sh
@@ -0,0 +1,78 @@
+# BE VERY CAREFULL EDITING THIS FILE!
+# There is a good chance your editor will mangle the characters used here.
+
+. "$(dirname "$0")/../commons.sh" 1>/dev/null
+
+PARAMETRIZE_HEAD_TYPE 'BRANCH' 'DETACH'
+PARAMETRIZE_APPLY_POP
+
+__test_section__ 'Prepare repository'
+printf 'foo\n' >'bo	=ÿþ€{}\*?#@![1;35;4;5m|:<>()^&[0mðŸ’©th'
+git add 'bo	=ÿþ€{}\*?#@![1;35;4;5m|:<>()^&[0mðŸ’©th'
+git commit -m 'Added a file'
+
+__test_section__ 'Create stash'
+printf 'bar\n' >'bo	=ÿþ€{}\*?#@![1;35;4;5m|:<>()^&[0mðŸ’©th'
+git add 'bo	=ÿþ€{}\*?#@![1;35;4;5m|:<>()^&[0mðŸ’©th'
+printf 'baz\n' >'bo	=ÿþ€{}\*?#@![1;35;4;5m|:<>()^&[0mðŸ’©th'
+printf 'zzz\n' >'bo	=ÿþ€{2}\*?#@![1;35;4;5m|:<>()^&[0mðŸ’©th'
+git stash push -u
+
+__test_section__ 'Create conflict'
+printf 'qux\n' >'bo	=ÿþ€{}\*?#@![1;35;4;5m|:<>()^&[0mðŸ’©th'
+printf 'yyy\n' >'bo	=ÿþ€{2}\*?#@![1;35;4;5m|:<>()^&[0mðŸ’©th'
+git add 'bo	=ÿþ€{}\*?#@![1;35;4;5m|:<>()^&[0mðŸ’©th' 'bo	=ÿþ€{2}\*?#@![1;35;4;5m|:<>()^&[0mðŸ’©th'
+git commit -m 'Changed the first file & added a second one'
+
+SWITCH_HEAD_TYPE
+
+__test_section__ "$CAP_OPERATION stash"
+correct_head_hash="$(get_head_hash_H)"
+assert_exit_code 2 capture_outputs git istash "$OPERATION"
+assert_conflict_message
+assert_files_H '
+UU bo\001\002\003\004\005\006\007\010\t=\377\376\177\200{}\\*?#@!\033[1;35;4;5m|:<>()^&\033[0m\360\237\222\251th qux|bar
+   bo\001\002\003\004\005\006\007\010\t=\377\376\177\200{2}\\*?#@!\033[1;35;4;5m|:<>()^&\033[0m\360\237\222\251th yyy
+!! ignored0	ignored0
+!! ignored1	ignored1
+'
+assert_stash_count 1
+assert_branch_count_H 1
+assert_data_files "$OPERATION"
+assert_rebase y
+
+__test_section__ "Continue $OPERATION stash (0)"
+printf 'quux\n' >'bo	=ÿþ€{}\*?#@![1;35;4;5m|:<>()^&[0mðŸ’©th'
+git add 'bo	=ÿþ€{}\*?#@![1;35;4;5m|:<>()^&[0mðŸ’©th'
+assert_exit_code 2 capture_outputs git istash "$OPERATION" --continue
+assert_conflict_message
+assert_files_H '
+UU bo\001\002\003\004\005\006\007\010\t=\377\376\177\200{}\\*?#@!\033[1;35;4;5m|:<>()^&\033[0m\360\237\222\251th quux|baz
+AA bo\001\002\003\004\005\006\007\010\t=\377\376\177\200{2}\\*?#@!\033[1;35;4;5m|:<>()^&\033[0m\360\237\222\251th yyy|zzz
+!! ignored0	ignored0
+!! ignored1	ignored1
+'
+assert_stash_count 1
+assert_branch_count_H 1
+assert_data_files "$OPERATION"
+assert_rebase y
+
+__test_section__ "Continue $OPERATION stash (1)"
+printf 'fff\n' >'bo	=ÿþ€{}\*?#@![1;35;4;5m|:<>()^&[0mðŸ’©th'
+printf 'xxx\n' >'bo	=ÿþ€{2}\*?#@![1;35;4;5m|:<>()^&[0mðŸ’©th'
+git add 'bo	=ÿþ€{}\*?#@![1;35;4;5m|:<>()^&[0mðŸ’©th' 'bo	=ÿþ€{2}\*?#@![1;35;4;5m|:<>()^&[0mðŸ’©th'
+assert_exit_code 0 git istash "$OPERATION" --continue
+assert_files_H '
+MM bo\001\002\003\004\005\006\007\010\t=\377\376\177\200{}\\*?#@!\033[1;35;4;5m|:<>()^&\033[0m\360\237\222\251th fff	quux
+ M bo\001\002\003\004\005\006\007\010\t=\377\376\177\200{2}\\*?#@!\033[1;35;4;5m|:<>()^&\033[0m\360\237\222\251th xxx	yyy
+!! ignored0	ignored0
+!! ignored1	ignored1
+'
+assert_stash_count_O 1
+assert_log_length_H 3
+assert_branch_count 1
+assert_head_hash_H "$correct_head_hash"
+assert_head_name_H
+assert_data_files 'none'
+assert_rebase n
+assert_branch_metadata_H
