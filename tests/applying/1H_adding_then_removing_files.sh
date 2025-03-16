0f69906be878f5b94828ba3a52219fde0aa41ac7
Piotr Siupa <piotr.siupa+stash@gmail.com>
2025-03-16 10:07:57 +0100

    Fixed a test that was failing on an orphan branch
    (The test was assuming there are preexisting files in a branch but that
    was no relevant to the tested behavior.)

--- tests/applying/1H_adding_then_removing_files.sh
+++ tests/applying/1H_adding_then_removing_files.sh
@@ -5,15 +5,10 @@ non_essential_test
 PARAMETRIZE_HEAD_TYPE 'BRANCH' 'DETACH' 'ORPHAN'
 PARAMETRIZE_APPLY_POP
 
-__test_section__ 'Prepare repository'
+__test_section__ 'Create stash'
 printf 'aaa\n' >aaa
 git add aaa
-git commit -m 'Added aaa'
-
-__test_section__ 'Create stash'
-printf 'bbb\n' >bbb
-git add bbb
-rm bbb
+rm aaa
 git istash push  # Normal "git stash" doesn't allow creation of such stash so "git istash" has to be used instead. (This is a special case in "git stash" for some weird reason.)
 
 SWITCH_HEAD_TYPE
@@ -22,13 +17,12 @@ __test_section__ "$CAP_OPERATION stash"
 correct_head_hash="$(get_head_hash_H)"
 assert_exit_code 0 git istash "$OPERATION"
 assert_files_H '
-   aaa		aaa
-AD bbb			bbb
+AD aaa		aaa
 !! ignored0	ignored0
 !! ignored1	ignored1
 '
 assert_stash_count_O 1
-assert_log_length_H 2
+assert_log_length_H 1
 assert_branch_count 1
 assert_head_hash_H "$correct_head_hash"
 assert_head_name_H
