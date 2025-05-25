715ed1d96e7d15097a0a3e8465a0b2273f9a4926
Piotr Siupa <piotr.siupa+stash@gmail.com>
2025-05-25 13:47:02 +0200

    Support for the options `--literal-pathspecs`
    Added tests and fixed the scripts for the cases when user calls it with
    the `GIT_LITERAL_PATHSPECS` set to true.

---
+++ tests/creating/5BQ_--literal-pathspecs.sh
@@ -0,0 +1,111 @@
+. "$(dirname "$0")/../commons.sh" 1>/dev/null
+
+PARAMETRIZE_HEAD_TYPE 'BRANCH' 'DETACH'
+PARAMETRIZE_ALL 'DEFAULT'
+PARAMETRIZE_UNTRACKED 'YES'
+PARAMETRIZE_KEEP_INDEX
+PARAMETRIZE_STAGED 'YES'
+PARAMETRIZE_UNSTAGED 'YES'
+PARAMETRIZE_PATHSPEC_STYLE
+PARAMETRIZE_OPTIONS_INDICATOR IS_PATHSPEC_IN_ARGS
+
+__test_section__ 'Prepare repository'
+printf 'xxx\n' >'f*'
+printf 'xxx\n' >'foo'
+printf 'xxx\n' >'bar'
+printf 'xxx\n' >'x*'
+printf 'xxx\n' >'xxx'
+git add .
+git commit -m 'Added a few files'
+
+correct_head_hash="$(get_head_hash)"
+SWITCH_HEAD_TYPE
+
+__test_section__ 'Create stash'
+printf 'yyy\n' >'f*'
+printf 'yyy\n' >'foo'
+printf 'yyy\n' >'bar'
+git add .
+printf 'yyy\n' >'x*'
+printf 'yyy\n' >'xxx'
+printf 'yyy\n' >'y*'
+printf 'yyy\n' >'yyy'
+printf 'f* x* y* ' | PREPARE_PATHSPEC_FILE
+if IS_PATHSPEC_IN_ARGS
+then
+	#shellcheck disable=SC2086
+	assert_exit_code 0 git --literal-pathspecs istash push $UNTRACKED_FLAGS $ALL_FLAGS $KEEP_INDEX_FLAGS $STAGED_FLAGS $UNSTAGED_FLAGS -m 'new stash' $EOI 'f*' 'x*' 'y*'
+elif IS_PATHSPEC_IN_STDIN
+then
+	#shellcheck disable=SC2086
+	assert_exit_code 0 git --literal-pathspecs istash push $UNTRACKED_FLAGS $ALL_FLAGS $KEEP_INDEX_FLAGS $STAGED_FLAGS $UNSTAGED_FLAGS -m 'new stash' $PATHSPEC_NULL_FLAGS --pathspec-from-file=- <.git/pathspec_for_test
+else
+	#shellcheck disable=SC2086
+	assert_exit_code 0 git --literal-pathspecs istash push $UNTRACKED_FLAGS $ALL_FLAGS $KEEP_INDEX_FLAGS $STAGED_FLAGS $UNSTAGED_FLAGS -m 'new stash' $PATHSPEC_NULL_FLAGS --pathspec-from-file .git/pathspec_for_test
+fi
+if ! IS_KEEP_INDEX_ON
+then
+	assert_files_H '
+	   f*		xxx
+	M  foo		yyy
+	M  bar		yyy
+	   x*		xxx
+	 M xxx		yyy	xxx
+	?? yyy		yyy
+	!! ignored0	ignored0
+	!! ignored1	ignored1
+	'
+else
+	assert_files_H '
+	M  f*		yyy
+	M  foo		yyy
+	M  bar		yyy
+	   x*		xxx
+	 M xxx		yyy	xxx
+	?? yyy		yyy
+	!! ignored0	ignored0
+	!! ignored1	ignored1
+	'
+fi
+assert_stash_H 0 'new stash' '
+M  f*		yyy
+   foo		xxx
+   bar		xxx
+ M x*		yyy	xxx
+   xxx		xxx
+?? y*		yyy
+'
+assert_stash_base_H 0 'HEAD'
+assert_stash_count 1
+assert_log_length_H 2
+assert_branch_count 1
+assert_head_hash_H "$correct_head_hash"
+assert_head_name_H
+assert_rebase n
+assert_branch_metadata_H
+assert_dotgit_contents
+
+git reset --hard
+RESTORE_HEAD_TYPE
+
+__test_section__ 'Pop stash'
+assert_exit_code 0 git stash pop --index
+assert_files '
+M  f*		yyy
+   foo		xxx
+   bar		xxx
+ M x*		yyy	xxx
+   xxx		xxx
+?? y*		yyy
+?? yyy		yyy
+!! ignored0	ignored0
+!! ignored1	ignored1
+'
+assert_stash_count 0
+assert_log_length 2
+assert_branch_count 1
+assert_head_hash "$correct_head_hash"
+assert_head_name 'master'
+assert_rebase n
+assert_branch_metadata_H
+assert_dotgit_contents
