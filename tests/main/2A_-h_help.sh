d9f6cd3297bd45dadd0996f92d25d9767dcb3ace
Piotr Siupa <piotr.siupa+stash@gmail.com>
2025-06-05 18:32:15 +0200

    `push` uses a new script `git-istash-make-stash`
    The new script is supposed to be a common core of all `push`-like and
    `create`-like commands. Currently it's a copy of the `git-istash-push`
    without some push-only code, but later it will be generalized a little.

---
+++ tests/main/2A_-h_help.sh
@@ -0,0 +1,26 @@
+# These are only a few rudimentary checks for things that are the easiest to forgot / mess up.
+# Always validate you're documentation manually.
+
+. "$(dirname "$0")/../commons.sh" 1>/dev/null
+
+PARAMETRIZE 'SUBCOMMAND' 'none' 'apply' 'pop' 'push'
+if [ "$SUBCOMMAND" = 'none' ]
+then
+	SUBCOMMAND=''
+fi
+
+#shellcheck disable=SC2086
+output="$(assert_exit_code 0 git istash $SUBCOMMAND -h)"
+mentions_of_this_subcommand="$(printf '%s\n' "$output" | grep -Fc "git istash $SUBCOMMAND")"
+mentions_of_any_subcommand="$(printf '%s\n' "$output" | grep -Ec 'git istash \w+' || true)"
+test "$mentions_of_this_subcommand" -ge 1 \
+	|| fail 'The "-h" help is not for the subcommand "%s"!\n' "$SUBCOMMAND"
+test "$mentions_of_this_subcommand" -ge "$mentions_of_any_subcommand" \
+	|| fail 'Other subcommands are mentioned in the "-h" help. (Did you copy it form other file?)\n'  # Not a real bug but currently the condition is fulfilled for all commands and this check will help to catch developer errors.
+usage_line_regex="^Usage: git istash $SUBCOMMAND"
+printf '%s\n' "$output" | grep -Eq "$usage_line_regex" \
+	|| fail 'The "-h" help doesn'\''t have a "usage" line!\n'
+printf '%s\n' "$output" | tail -n +3 | grep -Eq "$usage_line_regex" \
+	|| fail 'The "-h" help starts with the "usage" line!\n(There should be a short description.)\n'
+printf '%s\n' "$output" | grep -Fxq 'Options:' \
+	|| fail 'The "-h" help doesn'\''t have an "options" section!\n'
