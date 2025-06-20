f39a3be1bba0085d8e19ec914e264df0c9a48f5a
test <piotr.siupa+stash@gmail.com>
2025-06-20 21:49:57 +0200

    Added tests for the pseudo-internal options
    The options are `--no-store` and `--remove-stashed` and there are
    described in the manual as intended to be internal but allowed to be
    utilized by the users - so they have to be tested.

---
+++ tests/creating/5BW_switching_commands_with_--no-store.sh
@@ -0,0 +1,82 @@
+. "$(dirname "$0")/../commons.sh" 1>/dev/null
+
+PARAMETRIZE_HEAD_TYPE 'BRANCH' 'DETACH' 'ORPHAN'
+PARAMETRIZE_CREATE_OPERATION
+PARAMETRIZE_ALL 'YES'
+PARAMETRIZE_UNTRACKED 'DEFAULT' 'YES'
+PARAMETRIZE_KEEP_INDEX
+PARAMETRIZE_STAGED 'YES'
+PARAMETRIZE_UNSTAGED 'YES'
+
+correct_head_hash="$(get_head_hash)"
+SWITCH_HEAD_TYPE
+
+case "$CREATE_OPERATION" in
+	create) OPERATION_SWITCH_FLAGS='--no-store' ; CREATE_OPERATION_WITH_STORE='save' ;;
+	snatch) OPERATION_SWITCH_FLAGS='--no-store' ; CREATE_OPERATION_WITH_STORE='push' ;;
+	save) OPERATION_SWITCH_FLAGS='' ; CREATE_OPERATION_WITH_STORE='save' ;;
+	push) OPERATION_SWITCH_FLAGS='' ; CREATE_OPERATION_WITH_STORE='push' ;;
+esac
+
+__test_section__ "$CAP_CREATE_OPERATION stash"
+printf 'aaa\n' >aaa
+git add aaa
+printf 'bbb\n' >aaa
+printf 'ddd\n' >ddd
+#shellcheck disable=SC2086
+new_stash_hash_CO="$(assert_exit_code 0 git istash "$CREATE_OPERATION_WITH_STORE" $OPERATION_SWITCH_FLAGS $KEEP_INDEX_FLAGS $ALL_FLAGS $UNSTAGED_FLAGS $STAGED_FLAGS $UNTRACKED_FLAGS --message 'name of the new stash')"
+if ! IS_KEEP_INDEX_ON
+then
+	assert_files_HTCO '
+	AM aaa		bbb	aaa
+	?? ddd		ddd
+	!! ignored0	ignored0
+	!! ignored1	ignored1
+	' '
+	'
+else
+	assert_files_HTCO '
+	AM aaa		bbb	aaa
+	?? ddd		ddd
+	!! ignored0	ignored0
+	!! ignored1	ignored1
+	' '
+	A  aaa		aaa
+	'
+fi
+store_stash_CO "$new_stash_hash_CO"
+assert_stash_HTCO 0 'name of the new stash' '
+AM aaa		bbb	aaa
+?? ddd		ddd
+!! ignored0	ignored0
+!! ignored1	ignored1
+'
+assert_stash_base_HT 0 'HEAD'
+assert_stash_count 1
+assert_log_length_HT 1
+assert_branch_count 1
+assert_head_hash_HT "$correct_head_hash"
+assert_head_name_HT
+assert_rebase n
+assert_branch_metadata_HT
+assert_dotgit_contents
+
+remove_all_changes
+RESTORE_HEAD_TYPE
+
+__test_section__ 'Pop stash'
+assert_exit_code 0 git stash pop --index
+assert_files '
+AM aaa		bbb	aaa
+?? ddd		ddd
+!! ignored0	ignored0
+!! ignored1	ignored1
+'
+assert_stash_count 0
+assert_log_length 1
+assert_branch_count 1
+assert_head_hash "$correct_head_hash"
+assert_head_name 'master'
+assert_rebase n
+assert_branch_metadata_HT
+assert_dotgit_contents
