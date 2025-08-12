7bd36cb7b6c8c4fa70334242de640bd906fc13aa
Piotr Siupa <piotr.siupa+stash@gmail.com>
2025-08-12 21:42:28 +0200

    Checking operations in progress for creating
    Added tests for applying operations (`create`, `snatch`, `store` and
    `push`) to see if they correctly exit with an error (without breaking
    anything) when one of the following operations is currently in progress:
    another `istash apply`, `istash pop`, `rebase`, `merge`, `cherry-pick`,
    `revert` or `bisect`.

---
+++ tests/creating/5A_operation_already_in_progress.sh
@@ -0,0 +1,78 @@
+. "$(dirname "$0")/../commons.sh" 1>/dev/null
+
+non_essential_test
+
+PARAMETRIZE_HEAD_TYPE 'BRANCH' 'DETACH' 'ORPHAN'
+PARAMETRIZE_CREATE_OPERATION
+PARAMETRIZE_ALL 'DEFAULT'
+PARAMETRIZE_UNTRACKED 'DEFAULT'
+PARAMETRIZE_KEEP_INDEX 'DEFAULT'
+PARAMETRIZE_STAGED 'YES'
+PARAMETRIZE_UNSTAGED 'YES'
+
+__test_section__ 'Prepare repository'
+printf 'aaa\n' >aaa
+git add aaa
+git commit -m 'Added aaa'
+
+__test_section__ 'Create stash'
+printf 'bbb\n' >aaa
+git stash push
+
+__test_section__ 'Create conflict'
+printf 'ccc\n' >aaa
+git commit -am 'Changed aaa'
+
+SWITCH_HEAD_TYPE
+
+__test_section__ 'Apply stash'
+correct_head_hash_0="$(get_head_hash_HT)"
+assert_exit_code 2 capture_outputs git istash apply
+assert_files_HT '
+UU aaa		ccc|bbb
+!! ignored0	ignored0
+!! ignored1	ignored1
+' '
+DU aaa		bbb
+!! ignored0	ignored0
+!! ignored1	ignored1
+'
+assert_stash_count 1
+assert_rebase y
+assert_dotgit_contents_for 'apply'
+
+__test_section__ "$CAP_CREATE_OPERATION stash again"
+correct_head_hash_1="$(get_head_hash_HT)"
+assert_exit_code 1 git istash "$CREATE_OPERATION"
+assert_files_HT '
+UU aaa		ccc|bbb
+!! ignored0	ignored0
+!! ignored1	ignored1
+' '
+DU aaa		bbb
+!! ignored0	ignored0
+!! ignored1	ignored1
+'
+assert_stash_count 1
+assert_head_hash_HT "$correct_head_hash_1"
+assert_rebase y
+assert_dotgit_contents_for 'apply'
+
+__test_section__ 'Continue the first apply stash'
+printf 'ddd\n' >aaa
+git add aaa
+assert_exit_code 0 git istash apply --continue
+assert_files_HT '
+ M aaa		ddd	ccc
+!! ignored0	ignored0
+!! ignored1	ignored1
+' '
+ A aaa		ddd
+!! ignored0	ignored0
+!! ignored1	ignored1
+'
+assert_stash_count 1
+assert_head_hash_HT "$correct_head_hash_0"
+assert_rebase n
+assert_branch_metadata_HT
+assert_dotgit_contents
---
+++ tests/creating/5C_merge_already_in_progress.sh
@@ -0,0 +1,70 @@
+. "$(dirname "$0")/../commons.sh" 1>/dev/null
+
+non_essential_test
+
+PARAMETRIZE_HEAD_TYPE 'BRANCH' 'DETACH'
+PARAMETRIZE_CREATE_OPERATION
+PARAMETRIZE_ALL 'DEFAULT'
+PARAMETRIZE_UNTRACKED 'DEFAULT'
+PARAMETRIZE_KEEP_INDEX 'DEFAULT'
+PARAMETRIZE_STAGED 'YES'
+PARAMETRIZE_UNSTAGED 'YES'
+
+__test_section__ 'Prepare repository'
+git branch -m branch0
+printf 'aaa\n' >aaa
+git add aaa
+git commit -m 'Added aaa'
+
+__test_section__ 'Create stash'
+printf 'bbb\n' >aaa
+git stash push
+
+__test_section__ 'Create a commit to merge'
+git switch -c branch1
+git commit --allow-empty -m 'Changed nothing'
+
+git switch branch0
+SWITCH_HEAD_TYPE
+
+__test_section__ 'Merge branch'
+git merge branch1 --no-ff --no-commit
+assert_files_HT '
+   aaa		aaa
+!! ignored0	ignored0
+!! ignored1	ignored1
+'
+assert_stash_count 1
+assert_log_length_HT 2
+assert_branch_count 2
+assert_rebase n
+assert_dotgit_contents
+
+__test_section__ "$CAP_CREATE_OPERATION stash"
+correct_head_hash="$(get_head_hash_HT)"
+assert_exit_code 1 git istash "$CREATE_OPERATION"
+assert_files_HT '
+   aaa		aaa
+!! ignored0	ignored0
+!! ignored1	ignored1
+'
+assert_stash_count 1
+assert_log_length_HT 2
+assert_branch_count 2
+assert_head_hash_HT "$correct_head_hash"
+assert_rebase n
+assert_dotgit_contents
+
+__test_section__ "Continue merge"
+GIT_EDITOR='true' git merge --continue
+assert_files_HT '
+   aaa		aaa
+!! ignored0	ignored0
+!! ignored1	ignored1
+'
+assert_stash_count 1
+assert_log_length_HT 4
+assert_branch_count 2
+assert_rebase n
+assert_branch_metadata_HT
+assert_dotgit_contents
