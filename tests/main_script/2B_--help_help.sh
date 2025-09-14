f0eb17f6f033f519389748a9a01089ba49566cf7
Piotr Siupa <piotr.siupa+stash@gmail.com>
2025-09-14 13:36:24 +0200

    Added tests for `--help` and `--man`

---
+++ tests/main_script/2B_--help_help.sh
@@ -0,0 +1,18 @@
+# This only checks if the documentation is displayed, not it's contents.
+# Always validate you're documentation manually.
+
+. "$(dirname "$0")/../commons.sh" 1>/dev/null
+
+PARAMETRIZE_SUBCOMMAND
+
+
+__test_section__ "Displaying manual with \"$SUBCOMMAND --help\""
+printf '%s\n' '#/usr/bin/env sh' 'cd "$(dirname "$0")" || exit' 'printf '\''"%s"\n'\'' "$@" >'\''./call-to-man.txt'\' >'./man'
+chmod +x './man'
+PATH="$(pwd):$PATH"
+export PATH
+assert_exit_code 0 git istash "$SUBCOMMAND" --help
+test -f './call-to-man.txt' ||
+	fail '"man" was not called!\n'
+test "$(cat './call-to-man.txt')" = '"git-istash"' ||
+	fail '"man" was called with arguments "%s" instead of "%s"!\n' "$(cat './call-to-man.txt' | tr -d '"' | tr '\n' ' ' | sed 's/ $//')" 'git-istash'
