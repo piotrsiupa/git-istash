236abcfa77e2586613e448a11bbfd3d7df2d9429
Piotr Siupa <piotr.siupa+stash@gmail.com>
2026-01-03 14:13:37 +0100

    Multi-argument support in `tests/test-manager.sh`
    Added support for doing multiple tests at once, to not have to think
    about how the prefixes will change in the process and manually take it
    into account.

--- tests/test-manager.sh
+++ tests/test-manager.sh
@@ -6,17 +6,22 @@ print_help() {
 	printf 'A simple script that runs some fancy regexes and such to help with renaming\ntests in a way that keeps their name'\''s prefixes alphabetical.\n'
 	printf 'Unlike other tests scripts, this one works relatively to the current directory.\n'
 	printf '(The script assumes that existing tests have correct names and may misbehave if\nthey don'\''t.)\n'
+	printf '(It also assumes that there is no spaces or weird characters in the names.)\n'
+	printf '(And it also assumes that you always refer to the same folder in the same way,\nso e.g. it may mess up order of operation with creating files "./1C_first_test"\nand "1C_second_test".)\n'
 	printf '\n'
-	printf 'Usage: %s command [<options...>] [--] test_name\n' "$(basename "$0")"
+	printf 'Usage: %s <command> [<option>] [--] <test_name>...\n' "$(basename "$0")"
 	printf '\n'
-	printf 'General options:\n'
+	printf 'Options:\n'
 	printf '    -h, --help\t\t- Print this help message and exit.\n'
 	printf '\t--version\t- Print version information and exit.\n'
 	printf '\n'
 	printf 'Commands:\n'
-	printf '\tcreate\t- Add a new and empty test file with given name and rename other\n\t\t  tests to keep the prefixes ordered. '
-		printf 'You can skip the file\n\t\t  extension and if you want to just stick the test at the end of\n\t\t  a sub-category, you can skip the letters from the prefix too.\n'
-	printf '\tdelete\t- Remove the given test and rename other tests to keep the\n\t\t  prefixes ordered.\n'
+	printf '\tcreate\t- Add new and empty test files with given names and rename other\n\t\t  tests to keep the prefixes ordered. '
+		printf 'You can skip the file\n\t\t  extension and if you want to just stick the test at the end of\n\t\t  a sub-category, you can skip the letters from the prefix too.'
+		printf '\n\t\t  (The operations are ordered in such a way that tests will land\n\t\t  in the chosen spots even if the numeration has changed.'
+		printf '\n\t\t  If there are multiple tests for the same spot, they will\n\t\t  maintain their relative order.)\n'
+	printf '\tdelete\t- Remove the given test and rename other tests to keep the\n\t\t  prefixes ordered. '
+		printf '(The operations are ordered in such a way\n\t\t  that the correct tests are removed even if their names change\n\t\t  in the process.)\n'
 }
 
 print_version() {
@@ -63,130 +68,186 @@ decrement_prefix() { # prefix_or_test_name
 	printf '%s' "$1" | sed -E 's;^(.*/)?[0-9]+[A-Z]+(_[^/]+)?$;\2;'
 }
 
-create_test() { # new_test_name
-	if [ $# -lt 1 ]
-	then
-		printf 'You need to specify a test name to create!\n' 1>&2
-		return 1
-	elif [ $# -gt 1 ]
-	then
-		printf 'Only 1 argument is allowed!\n' 1>&2
-		return 1
-	fi
-	
-	category="$(extract_category "$1")"
-	last_test="$(list_category "$category" | sort -r | head -n 1)"
-	last_prefix="$(extract_prefix "$last_test" || true)"
-	if printf '%s' "$1" | grep -E -q '^(.*/)?[0-9]+[A-Z]+_[0-9a-zA_Z][^/]*$'
-	then
-		new_prefix="$(extract_prefix "$1")"
-		if [ -n "$last_prefix" ]
-		then
-			if [ "${#new_prefix}" -ne "${#last_prefix}" ]
-			then
-				printf 'The length of the new test'\''s prefix doesn'\''t match existing tests.\n' 1>&2
-				return 1
-			fi
-			if [ "$(printf '%s\n%s\n' "$new_prefix" "$(increment_prefix "$last_prefix")_" | sort -r | head -n1)" = "$new_prefix" ]
-			then
-				printf 'The prefix of the new test is too big; there would be a gap in tests.\n(The highest available prefix is "%s".)\n' "$(increment_prefix "$last_prefix")" 1>&2
-				return 1
-			fi
-		else
-			if [ "${#new_prefix}" != "${category}A" ]
-			then
-				printf 'The new prefix should be "%s" because it'\''s a new sub-category.\n' "${category}A" 1>&2
-				return 1
-			fi
-		fi
-		new_test_name="$1"
-	elif printf '%s' "$1" | grep -E -q '^(.*/)?[0-9]+_[0-9a-zA_Z][^/]*$'
-	then
-		if [ -n "$last_prefix" ]
-		then
-			new_prefix="$(increment_prefix "$last_prefix")"
-		else
-			new_prefix="${category}A"
-		fi
-		new_test_name="$(printf '%s' "$1" | sed -E 's;^(.*/)?[0-9]+(_[0-9a-zA_Z][^/]*)$;\1'"$new_prefix"'\2;')"
-	else
-		printf 'The test name has to contain at least the number of the sub-category and some text.\n'
-		return 1
-	fi
-	if ! printf '%s' "$new_test_name" | grep -E -q '\.sh$'
-	then
-		new_test_name="$new_test_name.sh"
-	fi
-	
-	if extract_prefix "$(list_category "$category" | sort -r | head -n1)" | grep -E -q -x '[0-9]+Z+'
-	then
-		new_prefix="$(elongate_prefix "$new_prefix")"
-		new_test_name="$(elongate_prefix "$new_test_name")"
-		list_category "$category" \
-		| while read -r test_to_rename
-		do
-			git mv "$test_to_rename" "$(elongate_prefix "$test_to_rename")"
-		done
-	fi
-	
-	{
-		printf '%s\n' "$(dirname "$new_test_name")/${new_prefix}_" | sed 'p'
-		list_category "$category"
-	} \
-	| sort -r \
-	| sed -E -n '1,/\/'"$new_prefix"'_$/p' \
-	| grep -v -F -x "$(dirname "$new_test_name")/${new_prefix}_" \
-	| while read -r test_to_rename
-	do
-		git mv "$test_to_rename" "$(increment_prefix "$test_to_rename")"
-	done
-	
-	touch "$new_test_name"
-	git add --intent-to-add "$new_test_name"
+sort_new_test_names() { # new_test_name...
+	printf '%s\n' "$@" \
+	| sed -n -E 's;^((.*/)?[0-9]+[A-Z]+)(_[0-9a-zA_Z][^/]*)$;\1 \3;p' \
+	| awk '
+		BEGIN {
+			count = 0
+		}
+		{
+			if ($1 in files_by_prefix)
+			{
+				files_by_prefix[$1] = $1 $2 "\n" files_by_prefix[$1]
+			}
+			else
+			{
+				files_by_prefix[$1] = $1 $2
+				for (i = count; i != 0 && prefixes[i-1] < $1; --i)
+				{
+					prefixes[i] = prefixes[i-1]
+				}
+				prefixes[i] = $1
+				++count
+			}
+		}
+		END {
+			for (i = 0; i != count; ++i)
+			{
+				print files_by_prefix[prefixes[i]]
+			}
+		}
+	'
+	printf '%s\n' "$@" \
+	| grep -E '^(.*/)?[0-9]+_[0-9a-zA_Z][^/]*$' || true
 }
 
-delete_test() { # test_name
+create_test() { # new_test_name...
 	if [ $# -lt 1 ]
 	then
-		printf 'You need to specify a test name to delete!\n' 1>&2
-		return 1
-	elif [ $# -gt 1 ]
-	then
-		printf 'Only 1 argument is allowed!\n' 1>&2
+		printf 'You need to specify at least one test name to create!\n' 1>&2
 		return 1
 	fi
 	
-	if [ ! -f "$1" ]
+	tests_with_bad_names="$(printf '%s\n' "$@" | grep -v -E '^(.*/)?[0-9]+[A-Z]*_[0-9a-zA_Z][^/]*$' || true)"
+	if [ -n "$tests_with_bad_names" ]
 	then
-		printf 'There'\''s no file "%s"!\n' "$1" 1>&2
+		printf 'The test name has to contain at least the number of the sub-category and some text.\n'
+		printf 'The following names are bad:\n'
+		#shellcheck disable=SC2086
+		printf -- ' - "%s"\n' $tests_with_bad_names
 		return 1
-	fi
+	fi 1>&2
 	
-	category="$(extract_category "$1")"
-	deleted_prefix="$(extract_prefix "$1")"
+	sorted_test_names="$(sort_new_test_names "$@")"
+	#shellcheck disable=SC2086
+	set -- $sorted_test_names
 	
-	git rm --force "$1"
-	
-	{
-		printf '%s\n' "$(dirname "$1")/${deleted_prefix}_" | sed 'p'
-		list_category "$category"
-	} \
-	| sort \
-	| sed -E '1,/\/'"$deleted_prefix"'_$/d' \
-	| grep -v -F -x "$(dirname "$1")/${deleted_prefix}_" \
-	| while read -r test_to_rename
+	while [ $# -ne 0 ]
 	do
-		git mv "$test_to_rename" "$(decrement_prefix "$test_to_rename")"
-	done
-	
-	if extract_prefix "$(list_category "$category" | sort -r | head -n1)" | grep -E -q '[0-9]A.'
-	then
-		list_category "$category" \
+		category="$(extract_category "$1")"
+		last_test="$(list_category "$category" | sort -r | head -n 1)"
+		last_prefix="$(extract_prefix "$last_test" || true)"
+		if printf '%s' "$1" | grep -E -q '^(.*/)?[0-9]+[A-Z]+_[0-9a-zA_Z][^/]*$'
+		then
+			new_prefix="$(extract_prefix "$1")"
+			if [ -n "$last_prefix" ]
+			then
+				if [ "${#new_prefix}" -ne "${#last_prefix}" ]
+				then
+					printf 'The length of the new test'\''s prefix doesn'\''t match existing tests.\n' 1>&2
+					return 1
+				fi
+				if [ "$(printf '%s\n%s\n' "$new_prefix" "$(increment_prefix "$last_prefix")_" | sort -r | head -n1)" = "$new_prefix" ]
+				then
+					printf 'The prefix of the new test is too big; there would be a gap in tests.\n(The highest available prefix is "%s".)\n' "$(increment_prefix "$last_prefix")" 1>&2
+					return 1
+				fi
+			else
+				if [ "${#new_prefix}" != "${category}A" ]
+				then
+					printf 'The new prefix should be "%s" because it'\''s a new sub-category.\n' "${category}A" 1>&2
+					return 1
+				fi
+			fi
+			new_test_name="$1"
+		elif printf '%s' "$1" | grep -E -q '^(.*/)?[0-9]+_[0-9a-zA_Z][^/]*$'
+		then
+			if [ -n "$last_prefix" ]
+			then
+				new_prefix="$(increment_prefix "$last_prefix")"
+			else
+				new_prefix="${category}A"
+			fi
+			new_test_name="$(printf '%s' "$1" | sed -E 's;^(.*/)?[0-9]+(_[0-9a-zA_Z][^/]*)$;\1'"$new_prefix"'\2;')"
+		fi
+		if ! printf '%s' "$new_test_name" | grep -E -q '\.sh$'
+		then
+			new_test_name="$new_test_name.sh"
+		fi
+		
+		if extract_prefix "$(list_category "$category" | sort -r | head -n1)" | grep -E -q -x '[0-9]+Z+'
+		then
+			new_prefix="$(elongate_prefix "$new_prefix")"
+			new_test_name="$(elongate_prefix "$new_test_name")"
+			list_category "$category" \
+			| while read -r test_to_rename
+			do
+				git mv "$test_to_rename" "$(elongate_prefix "$test_to_rename")"
+			done
+		fi
+		
+		{
+			printf '%s\n' "$(dirname "$new_test_name")/${new_prefix}_" | sed 'p'
+			list_category "$category"
+		} \
+		| sort -r \
+		| sed -E -n '1,/\/'"$new_prefix"'_$/p' \
+		| grep -v -F -x "$(dirname "$new_test_name")/${new_prefix}_" \
 		| while read -r test_to_rename
 		do
-			git mv "$test_to_rename" "$(infinitesimalize_prefix "$test_to_rename")"
+			git mv "$test_to_rename" "$(increment_prefix "$test_to_rename")"
 		done
+		
+		touch "$new_test_name"
+		git add --intent-to-add "$new_test_name"
+		
+		shift
+	done
+}
+
+delete_test() { # test_name...
+	if [ $# -lt 1 ]
+	then
+		printf 'You need to specify at least one test name to delete!\n' 1>&2
+		return 1
 	fi
+	
+	(
+		while [ $# -ne 0 ]
+		do
+			if [ ! -f "$1" ]
+			then
+				printf 'There'\''s no file "%s"!\n' "$1" 1>&2
+				return 1
+			fi
+			shift
+		done
+	)
+	
+	#shellcheck disable=SC2046
+	set -- $(printf '%s\n' "$@" | sort -r -u)
+	
+	while [ $# -ne 0 ]
+	do
+		category="$(extract_category "$1")"
+		deleted_prefix="$(extract_prefix "$1")"
+		
+		git rm --force "$1"
+		
+		{
+			printf '%s\n' "$(dirname "$1")/${deleted_prefix}_" | sed 'p'
+			list_category "$category"
+		} \
+		| sort \
+		| sed -E '1,/\/'"$deleted_prefix"'_$/d' \
+		| grep -v -F -x "$(dirname "$1")/${deleted_prefix}_" \
+		| while read -r test_to_rename
+		do
+			git mv "$test_to_rename" "$(decrement_prefix "$test_to_rename")"
+		done
+		
+		if extract_prefix "$(list_category "$category" | sort -r | head -n1)" | grep -E -q '[0-9]A.'
+		then
+			list_category "$category" \
+			| while read -r test_to_rename
+			do
+				git mv "$test_to_rename" "$(infinitesimalize_prefix "$test_to_rename")"
+			done
+		fi
+		
+		shift
+	done
 }
 
 getopt_short_options='hs'
