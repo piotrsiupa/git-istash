ac6703760e25a67979332865764ed68d07399090
Piotr Siupa <piotr.siupa+stash@gmail.com>
2026-01-28 06:43:46 +0100

    Replaced an irrelevant comment in a test
    The plan was to add an explanatory comment for why there are no tests
    for other user errors in regard to the list of arguments. However, it
    turned out that there is already a comment there, that was accidentally
    copied from another test.

--- tests/main_script/1B_invalid_option.sh
+++ tests/main_script/1B_invalid_option.sh
@@ -1,5 +1,5 @@
-# These are only a few rudimentary checks for things that are the easiest to forgot / mess up.
-# Always validate you're documentation manually.
+# This checks if the error message is passed to the user correctly rather that checking the argument validation in general.
+# That's why there are no tests for other kind of errors here. (They are all handled in unit tests.)
 
 . "$(dirname "$0")/../commons.sh" 1>/dev/null
 
