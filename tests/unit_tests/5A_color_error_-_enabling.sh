fe19aa8453ccff155908f8ec4e8a142097d7a516
Piotr Siupa <piotr.siupa+stash@gmail.com>
2026-04-03 18:30:55 +0200

    Fixed some mistakes in existing unit tests

--- tests/unit_tests/5A_color_error_-_enabling.sh
+++ tests/unit_tests/5A_color_error_-_enabling.sh
@@ -14,6 +14,11 @@ EOF
 chmod +x 'color_error.sh'
 ln -s "$(cd - 1>/dev/null ; pwd)/../lib/git-istash/get_options" ./
 
+__test_section__ 'always'
+git config --local color.ui always
+assert_exit_code 0 './color_error.sh'
+assert_outputs '\[31mfoo\[0?m' ''
+
 __test_section__ 'never'
 git config --local color.ui never
 assert_exit_code 0 './color_error.sh'
@@ -22,7 +27,7 @@ assert_outputs 'foo' ''
 __test_section__ 'never <- always'
 git config --local color.istash always
 assert_exit_code 0 './color_error.sh'
-assert_outputs '\[31mfoo\[0?m'
+assert_outputs '\[31mfoo\[0?m' ''
 
 __test_section__ 'never <- never'
 git config --local color.istash never
@@ -32,7 +37,7 @@ assert_outputs 'foo' ''
 __test_section__ 'never <- never <- always'
 git config --local color.istash.error always
 assert_exit_code 0 './color_error.sh'
-assert_outputs '\[31mfoo\[0?m'
+assert_outputs '\[31mfoo\[0?m' ''
 
 __test_section__ 'never <- never <- never'
 git config --local color.istash.error never
