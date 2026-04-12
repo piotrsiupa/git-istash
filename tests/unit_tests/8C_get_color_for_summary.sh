9a5ab46a363bbe6d1b46962e0d46e3ed675f251d
Piotr Siupa <piotr.siupa+stash@gmail.com>
2026-04-12 21:05:05 +0200

    Changed how `color.istash.added`
    This is supposed to work in the same way as `color.status.added` and
    `color.status.updated`. Now `color.istash.added` is just a fallback
    value for `color.istash.updated`.

--- tests/unit_tests/8C_get_color_for_summary.sh
+++ tests/unit_tests/8C_get_color_for_summary.sh
@@ -17,8 +17,6 @@ ln -s "$(cd - 1>/dev/null ; pwd)/../lib/git-istash/get_options" ./
 git config --local color.ui always
 
 __test_section__ 'none set'
-assert_exit_code 0 './get_color_for_summary.sh' 'added'
-assert_outputs '\[32m' ''
 assert_exit_code 0 './get_color_for_summary.sh' 'updated'
 assert_outputs '\[32m' ''
 assert_exit_code 0 './get_color_for_summary.sh' 'changed'
@@ -28,11 +26,11 @@ assert_outputs '\[31m' ''
 
 __test_section__ 'only vanilla set'
 git config --local color.status.added 'blue'
+assert_exit_code 0 './get_color_for_summary.sh' 'updated'
+assert_outputs '\[34m' ''
 git config --local color.status.updated 'cyan'
 git config --local color.status.changed 'yellow'
 git config --local color.status.untracked 'magenta'
-assert_exit_code 0 './get_color_for_summary.sh' 'added'
-assert_outputs '\[34m' ''
 assert_exit_code 0 './get_color_for_summary.sh' 'updated'
 assert_outputs '\[36m' ''
 assert_exit_code 0 './get_color_for_summary.sh' 'changed'
@@ -42,11 +40,11 @@ assert_outputs '\[35m' ''
 
 __test_section__ 'both set'
 git config --local color.istash.summary.added 'bold red'
+assert_exit_code 0 './get_color_for_summary.sh' 'updated'
+assert_outputs '\[1;31m' ''
 git config --local color.istash.summary.updated 'black'
 git config --local color.istash.summary.changed 'bold yellow'
 git config --local color.istash.summary.untracked 'green'
-assert_exit_code 0 './get_color_for_summary.sh' 'added'
-assert_outputs '\[1;31m' ''
 assert_exit_code 0 './get_color_for_summary.sh' 'updated'
 assert_outputs '\[30m' ''
 assert_exit_code 0 './get_color_for_summary.sh' 'changed'
@@ -59,8 +57,6 @@ git config --local --unset color.status.added
 git config --local --unset color.status.updated
 git config --local --unset color.status.changed
 git config --local --unset color.status.untracked
-assert_exit_code 0 './get_color_for_summary.sh' 'added'
-assert_outputs '\[1;31m' ''
 assert_exit_code 0 './get_color_for_summary.sh' 'updated'
 assert_outputs '\[30m' ''
 assert_exit_code 0 './get_color_for_summary.sh' 'changed'
