8ad1b43e7f550ac18ed83b1569bd4295a6667021
Piotr Siupa <piotr.siupa+stash@gmail.com>
2026-05-27 23:51:06 +0200

    Added an option `--quickie` to `tests/monitor.sh`
    I was constantly forgetting that it's not supported in this script and
    you have to use `-m` for it. This was annoying.

--- tests/monitor.sh
+++ tests/monitor.sh
@@ -18,6 +18,7 @@ print_help() {
 	printf '    -m, --meticulous=X\t- Set how many tests / test variants will be run.\n\t\t\t  (For more info, run "run.sh --help".)\n'
 	printf '    -s, --skip-init\t- Skip the initial run that checks which tests fail.\n\t\t\t  (Assume that the relevant tests has failed already.)\n'
 	printf '    -S, --stop-at-fail\t- Don'\''t find all failing tests first. Go to the fixing\n\t\t\t  mode after encountering the first one.\n\t\t\t  (Good when expecting a lot of errors.)\n'
+	printf '\t--quickie\t- Same as "--meticulousness=quickie".\n'
 	printf '    -V, --version\t- Print version information and exit.\n'
 	printf '\n'
 	printf 'For info about filters, run "run.sh --help".\n'
@@ -138,7 +139,7 @@ monitor_tests() { # [filter]...
 }
 
 getopt_short_options='aA:c:hm:sSV'
-getopt_long_options='altered,since:,color:,help,meticulousness:,skip-init,stop-at-fail,stop-on-fail,stop-at-error,stop-on-error,version'
+getopt_long_options='altered,since:,color:,help,meticulousness:,skip-init,stop-at-fail,stop-on-fail,stop-at-error,stop-on-error,quickie,version'
 normalized_options="$(getopt -o"$getopt_short_options" --long="$getopt_long_options" -n"${0##*/}" -ssh -- "$@")"
 eval set -- "$normalized_options"
 only_altered=n
@@ -194,6 +195,11 @@ do
 		;;
 	-S|--stop-at-fail|--stop-on-fail|--stop-at-error|--stop-on-error)
 		stop_at_fail=y
+		;;
+	--quickie)
+		shift
+		set -- '--meticulousness' 'quickie' "$@"
+		continue
 		;;
 	-V|--version)
 		print_version
